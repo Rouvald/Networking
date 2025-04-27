140f0a5d193751ee84bd1580d2694749768a0b53
Rouvald <viktor.a2202@gmail.com>
2025-04-27 23:23:10 +0300

    [Win WIP] Update conan integration and tested only for Win

--- compile.sh
+++ compile.sh
@@ -3,6 +3,7 @@
 build_type="Release"
 build_dir="build"
 cmake_generator="Ninja"
+conan_profile_dir="conanProfiles"
 conan_profile="conanProfileRelease"
 
 case $1 in
@@ -17,17 +18,19 @@ esac
 if [[ "${OSTYPE}" == "linux-gnu" ]];
 then
     build_dir="build_${build_type}"
-    cmake_generator="Unix Makefiles"
+    cmake_generator="Ninja"
+    conan_profile="conanProfile${build_type}_Linux"
 elif [[ "${OSTYPE}" == "msys" ]]; 
 then
     build_dir="build_win_${build_type}"
     cmake_generator="Ninja"
+    conan_profile="conanProfile${build_type}_Win"
 fi
 
 rm -rf "${build_dir}"
 mkdir "${build_dir}"
 
-conan install . --profile=${conan_profile} --output-folder=""${build_dir}"" --build=missing
+conan install . --profile="${conan_profile_dir}/${conan_profile}" --profile:b="${conan_profile_dir}/${conan_profile}" --output-folder="${build_dir}" --build=missing
 
 cmake -DCMAKE_BUILD_TYPE:STRING=${build_type} -DCMAKE_CXX_COMPILER=g++ -S "." -B "${build_dir}" -G "${cmake_generator}"
 cmake --build "${build_dir}" --config ${build_type} --target clean -j 18 --
--- devops/githooks/install_git_hooks.sh
+++ devops/githooks/install_git_hooks.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-cp -r /hooks/ ../../.git/
+cp -r hooks/ ../../.git/
