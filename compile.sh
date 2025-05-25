37463ed93c4708da33c6b92a8a7e338f04f77223
Rouvald <viktor.a2202@gmail.com>
2025-05-25 23:47:13 +0300

    [WIN WIP] Adding selection for using conan manager

--- compile.sh
+++ compile.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+set -e
+
 build_type="Release"
 build_dir="build"
 cmake_generator="Ninja"
 conan_profile_dir="conanProfiles"
 conan_profile="conanProfileRelease"
+use_conan="ON"
 
-case $1 in
-    -d )
-    build_type="Debug"
-    ;;
-    -r )
-    build_type="Release"
-    ;;
-esac
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    -d ) build_type="Debug" ;;
+    -r ) build_type="Release" ;;
+    --no-conan ) use_conan="OFF" ;;
+    *) echo "Unknown arg: $1"; exit 1 ;;
+  esac
+  shift
+done
 
 os_name="$(uname -s)"
 
@@ -32,8 +36,20 @@ fi
 rm -rf "${build_dir}"
 mkdir "${build_dir}"
 
-conan install . --profile="${conan_profile_dir}/${conan_profile}" --profile:b="${conan_profile_dir}/${conan_profile}" --output-folder="${build_dir}" --build=missing
+if [[ "$use_conan" == "ON" ]]; then
+    conan install . \
+        --profile="${conan_profile_dir}/${conan_profile}" \
+        --profile:b="${conan_profile_dir}/${conan_profile}" \
+        --output-folder="${build_dir}" \
+        --build=missing
+fi
 
-cmake -DCMAKE_BUILD_TYPE:STRING=${build_type} -DCMAKE_CXX_COMPILER=g++ -S "." -B "${build_dir}" -G "${cmake_generator}"
+cmake -DCMAKE_BUILD_TYPE:STRING=${build_type} \
+      -DCMAKE_CXX_COMPILER=g++ \
+      -S "." \
+      -B "${build_dir}" \
+      -G "${cmake_generator}" \
+      -DUSE_CONAN=${use_conan}
+
 cmake --build "${build_dir}" --config ${build_type} --target clean -j 18 --
 cmake --build "${build_dir}" --config ${build_type} --target all -j 18 --
